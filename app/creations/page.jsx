384dd0443e8cbe958e9d29ff7bc568877b4ebe33
Jordy <jordycorporan95@gmail.com>
2024-03-10 22:50:02 -0400

    used dummy json data for sample display and added card block

--- app/creations/page.jsx
+++ app/creations/page.jsx
@@ -1,7 +1,22 @@
-
+import creations from '@/creations.json'
+import CreationBlock from '@/components/CreationBlock'
 const CreationsPage = () => {
   return (
-    <div>CreationsPage</div>
+    <section className='px-4 py-6'>
+      <div className='container-xl lg:container m-auto px-4 py-6'>
+        {creations.length === 0 ? (
+          <div className='text-center col-span-3'>
+            <p className='text-gray-500'>No beautiful creations found</p>
+          </div>
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {creations.map((creation) => (
+              <CreationBlock key={creation.id} creation={creation} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
---
+++ components/CreationBlock.jsx
@@ -0,0 +1,56 @@
+import Image from "next/image"
+import Link from "next/link"
+import { FaPalette, FaRuler, FaUser } from "react-icons/fa"
+
+const CreationBlock = ({ creation }) => {
+    return (
+      <div className='rounded-xl shadow-md relative'>
+        <Image
+          src={creation.images[0]}
+          alt={creation.title}
+          style={{ aspectRatio: '1/1'}}
+          sizes="100vw"
+          width={0}
+          height={0}
+          className='w-full rounded-t-xl'
+        />
+        <div className='p-4'>
+          <div className='text-left md:text-center lg:text-left mb-6'>
+            <div className='text-gray-600'>{creation.category}</div>
+            <h3 className='text-xl font-bold'>{creation.title}</h3>
+          </div>
+          <h3 className='absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right'>
+            ${creation.price.amount}
+          </h3>
+
+          <div className='flex justify-center gap-4 text-gray-500 mb-4'>
+            <p>
+             <FaPalette/> {creation.details.material}
+            </p>
+            <p>
+             <FaRuler/> {creation.details.dimensions}
+            </p>
+          </div>
+
+          <div className='border border-gray-100 mb-5'></div>
+
+          <div className='flex flex-col lg:flex-row justify-between mb-4'>
+            <div className='flex align-middle gap-2 mb-4 lg:mb-0'>
+
+              <FaUser className='text-lg text-orange-700' />
+              <span className='text-orange-700'>{creation.artisan_info.name} | </span>
+              <span className='text-orange-700'>{creation.artisan_info.location}</span>
+            </div>
+            <Link
+              href={`/creations/${creation._id}`}
+              className='h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm'
+            >
+              View Craft
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  export default CreationBlock
