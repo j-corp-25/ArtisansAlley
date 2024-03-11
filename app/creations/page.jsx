81f2f29244bbff42b16698e3e7640d67b7a595db
Jordy <jordycorporan95@gmail.com>
2024-03-11 14:49:44 -0400

    remove console logs

--- app/creations/page.jsx
+++ app/creations/page.jsx
@@ -19,7 +19,6 @@ const fetchCreations = async () =>{
 
 const CreationsPage = async () => {
   const creations = await fetchCreations()
-  console.log(creations)
   return (
     <section className='px-4 py-6'>
       <div className='container-xl lg:container m-auto px-4 py-6'>
