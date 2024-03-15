8201208073444e9a3db8c6a310916551dfb12ef7
Jordy <jordycorporan95@gmail.com>
2024-03-14 21:42:01 -0400

    sign in working

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from 'next-auth/providers/google'
+import connectDB from '@/config/db'
+import User from '@/models/User'
 
 export const authOptions = {
   providers: [
@@ -14,12 +16,26 @@ export const authOptions = {
       },
     }),
   ],
-  callbacks:{
-    async signIn({ profile}) {
-
+  callbacks: {
+    async signIn({ profile }) {
+      await connectDB()
+      const user = await User.findOne({ email: profile.email })
+      const username = profile.name.slice(0,20)
+      if (!user) {
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        })
+      }
+      return true
     },
-    async session ({session}){
-        
-    }
-  }
+    async session({ session }) {
+      const user = await User.findOne({ email: session.user.email })
+      if (user) {
+        session.user.id = user._id.toString()
+      }
+      return session
+    },
+  },
 }
